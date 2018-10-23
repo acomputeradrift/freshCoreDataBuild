de7c67a9f024253a28805fde663bf32289ec0812
Jamie Feeny <feeny.jamie@gmail.com>
2018-10-23 12:20:58 -0700

    Creating a category and retreiving it from CoreData to display works

--- freshCoreDataBuild/Controller/CategoryTableViewController.swift
+++ freshCoreDataBuild/Controller/CategoryTableViewController.swift
@@ -17,30 +17,29 @@ class CategoryTableViewController: UITableViewController {
         super.viewDidLoad()
         
         
-        let context = AppDelegate.viewContext
-        
-        if categories.count == 0{
-            let category = Category(context:context)
-            category.name = "To Be Categorized"
-            
-            let subCategory = SubCategory(context:category.managedObjectContext!)
-            subCategory.name = "-"
-            
-            category.addToChildren(subCategory)
-            do {
-                try context.save()
-                //categories.append(category )
-            } catch let error as NSError {
-                print("Could not save. \(error), \(error.userInfo)")
-            }
-        }
-        let request =
-            NSFetchRequest<NSManagedObject>(entityName: "Category")
-        request.sortDescriptors = [NSSortDescriptor(key: "name", ascending: true)]
-        categories = try! context.fetch(request) as! [Category]
-        print(categories)
-        //let sortDescriptor = NSSortDescriptor(key: "name", ascending: true)
-        //hard code a default item into the persistant container - separate function
+//        let context = AppDelegate.viewContext
+        print(categories.count)
+//        if categories.count == 0{
+//            let category = Category(context:context)
+//            category.name = "To Be Categorized"
+//
+//            let subCategory = SubCategory(context:category.managedObjectContext!)
+//            subCategory.name = "-"
+//
+//            category.addToChildren(subCategory)
+//            do {
+//                try context.save()
+//                //categories.append(category )
+//            } catch let error as NSError {
+//                print("Could not save. \(error), \(error.userInfo)")
+//            }
+//        let request =
+//            NSFetchRequest<NSManagedObject>(entityName: "Category")
+//        request.sortDescriptors = [NSSortDescriptor(key: "name", ascending: true)]
+//        categories = try! context.fetch(request) as! [Category]
+//        }
+//        print(categories)
+    
 
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
@@ -71,7 +70,58 @@ class CategoryTableViewController: UITableViewController {
 
         return cell
     }
+    
+    func createCategory(categoryName: String){
+        let context = AppDelegate.viewContext
+        let category = Category(context:context)
+        category.name = categoryName
+        do {
+            try context.save()
+            //categories.append(category)
+        } catch let error as NSError {
+            print("Could not save. \(error), \(error.userInfo)")
+        }
+    }
+    
+    func retrieveCategories(){
+        let context = AppDelegate.viewContext
+        let request =
+            NSFetchRequest<NSManagedObject>(entityName: "Category")
+        request.sortDescriptors = [NSSortDescriptor(key: "name", ascending: true)]
+        categories = try! context.fetch(request) as! [Category]
+    }
+    
+    
+    func updateCategory(){
+       // let context = AppDelegate.viewContext
+    }
+    
+    func deleteCategory(){
+        //let context = AppDelegate.viewContext
+    }
  
+    @IBAction func addName(_ sender: UIBarButtonItem) {
+        let alert = UIAlertController(title: "New Category",
+                                      message: "Add a new category",
+                                      preferredStyle: .alert)
+        let saveAction = UIAlertAction(title: "Save", style: .default) {
+            [unowned self] action in
+            
+            guard let textField = alert.textFields?.first,
+                let categoryName = textField.text else {
+                    return
+            }
+            self.createCategory(categoryName: categoryName)
+            self.retrieveCategories()
+            self.tableView.reloadData()
+        }
+        let cancelAction = UIAlertAction(title: "Cancel",
+                                         style: .cancel)
+        alert.addTextField()
+        alert.addAction(saveAction)
+        alert.addAction(cancelAction)
+        present(alert, animated: true)
+    }
 
     /*
     // Override to support conditional editing of the table view.
